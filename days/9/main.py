0935da2a86abe89cf63996a7d7ea6e52235e3d84
Dean Kerr <dean_k@live.co.uk>
2023-12-11 20:43:12 +0000

    Tidy day nine implementation

--- days/9/main.py
+++ days/9/main.py
@@ -1,15 +1,15 @@
-def derive_sequence(sequence: list[int]) -> int:
+def derive_sequence(sequence: list[int], extrapolate_forwards: bool) -> int:
     sequence_inner = [element - sequence[index] for index, element in enumerate(sequence[1:])]
-    print('derive sequence', sequence, sequence_inner)
 
     if all([element == 0 for element in sequence_inner]):
-        print('result', sequence_inner[0])
-        return sequence[0] - sequence_inner[0]
+        if extrapolate_forwards:
+            return sequence[-1] + sequence_inner[0]
+        return sequence[0] - sequence_inner[-1]
 
-
-    result = sequence[0] - derive_sequence(sequence_inner)
-    print('result', result)
-    return result
+    if extrapolate_forwards:
+        return sequence[-1] + derive_sequence(sequence_inner, extrapolate_forwards)
+    else:
+        return sequence[0] - derive_sequence(sequence_inner, extrapolate_forwards)
 
 
 if __name__ == '__main__':
@@ -17,10 +17,6 @@ if __name__ == '__main__':
         sequences = [list(map(int, line.strip().split(' '))) for line in file.readlines()]
         file.close()
 
-    print(sequences)
+    print('Part one', sum([derive_sequence(sequence, True) for sequence in sequences]))
+    print('Part two', sum([derive_sequence(sequence, False) for sequence in sequences]))
 
-    result = derive_sequence(sequences[2])
-
-    print(result)
-
-    print(sum([derive_sequence(sequence) for sequence in sequences]))
